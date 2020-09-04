49edd0c8cbca2b23e4ebdeff37d8c6e01b0dbe28
iZavadskiy <to.zavadskiy@gmail.com>
2020-09-04 05:55:25 +0300

    init commit

---
+++ precache-manifest.537534a6f90810b5e6914ddff1f8dac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3144972b6b43ff63fc90c533aa7d3350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11a5598ccd534699e50d",
+    "url": "/static/css/main.12825de5.chunk.css"
+  },
+  {
+    "revision": "feb14c8112ce9b4ff5c8",
+    "url": "/static/js/2.ab3737ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ab3737ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a5598ccd534699e50d",
+    "url": "/static/js/main.77d51e4e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
